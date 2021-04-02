ad7ba0766ba48efe3457722d3e088cf4d7693850
Юля <ymakarevich76@gmail.com>
2021-04-02 16:31:16 +0300

    Create blocks and slider

---
+++ _src/static/script/block/__slider.js
@@ -0,0 +1,10 @@
+new Swiper('.opinion__slider', {
+  //Стрелки
+  navigation: {
+    nextEl: '.opinion__slider-b-next',
+    prevEl: '.opinion__slider-b-prev'
+  },
+ autoHeigth: true,
+ loop: true,
+ loopedSledies: 3,
+});
