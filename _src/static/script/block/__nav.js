a352e0c4dba1f33cf8b5761b37a06556ab21a548
Юля <ymakarevich76@gmail.com>
2021-06-11 11:40:26 +0300

    Correct background in header

--- _src/static/script/block/__nav.js
+++ _src/static/script/block/__nav.js
@@ -1,5 +1,6 @@
 const burgerBtn = document.querySelector('.header__container-burger');
 const nav = document.querySelector('.nav');
+const link = document.querySelector('.nav__item-link--active')
 
 burgerBtn.addEventListener('click', () => {
   if(nav.classList.contains('nav--none')) {
@@ -7,6 +8,7 @@ burgerBtn.addEventListener('click', () => {
     nav.classList.add('nav--block');
     burgerBtn.classList.remove('header__container-burger--opened');
     burgerBtn.classList.add('header__container-burger--closed');
+    link.setAttribute('style', 'border: 1px solid transparent;');
   } else {
     nav.classList.add('nav--none');
     nav.classList.remove('nav--block');
